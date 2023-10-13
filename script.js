634abb80ece25f0546f75a5eef3ac554030722a7
SujanDevaraj <96370355+SujanDevaraj@users.noreply.github.com>
2023-10-13 21:11:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// complete this js code
 // Constructor function for Person
 function Person(name, age) {
   this.name = name;
@@ -26,6 +25,5 @@ Employee.prototype.jobGreet = function() {
   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 };
 
-// Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
