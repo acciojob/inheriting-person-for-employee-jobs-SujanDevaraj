61b9baf73a3794178ab4e4bbb14f5bdf29ebacab
SujanDevaraj <96370355+SujanDevaraj@users.noreply.github.com>
2023-10-13 21:16:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,25 @@
-// Constructor function for Person
+// complete this js code
 function Person(name, age) {
   this.name = name;
   this.age = age;
 }
-
-// Add a method to the Person prototype
 Person.prototype.greet = function() {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 };
 
-// Constructor function for Employee
+
 function Employee(name, age, jobTitle) {
-  // Call the constructor of the parent class (Person)
   Person.call(this, name, age);
-  
   this.jobTitle = jobTitle;
 }
 
-// Inherit the Person prototype in the Employee prototype
 Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
 
-// Add a method to the Employee prototype
 Employee.prototype.jobGreet = function() {
   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 };
 
+// Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
-person.greet();
-employee.greet(); // Outputs: Hello, my name is Bob, I am 25 years old.
-employee.jobGreet(); // Outputs: Hello, my name is Bob, I am 25 years old, and my job title is Software Engineer.
